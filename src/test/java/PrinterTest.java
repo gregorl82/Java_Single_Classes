73b7b5eea1920e0f286eddb5ae74554cafb634a9
Gregor Lennie <gregorlennie@hotmail.co.uk>
2020-01-27 16:13:38 +0000

    Adds JUnit tests for Printer class

--- src/test/java/PrinterTest.java
+++ src/test/java/PrinterTest.java
@@ -1,2 +1,42 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class PrinterTest {
+
+    private Printer printer;
+
+    @Before
+    public void before(){
+        printer = new Printer(20);
+    }
+
+    @Test
+    public void hasPaper(){
+        assertEquals(20, printer.getPaper());
+    }
+
+    @Test
+    public void canPrint(){
+        printer.print(3, 2);
+        assertEquals(14, printer.getPaper());
+    }
+
+    @Test
+    public void notEnoughPaper(){
+        printer.print(6, 4);
+        assertEquals(20, printer.getPaper());
+    }
+
+    @Test
+    public void hasToner(){
+        assertEquals(20, printer.getToner());
+    }
+
+    @Test
+    public void printingReducesToner(){
+        printer.print(1, 1);
+        assertEquals(19, printer.getToner());
+    }
 }
