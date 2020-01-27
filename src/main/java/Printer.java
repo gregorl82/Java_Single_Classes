35d5a615f20addba6b28d2d953d3f0739aa51de9
Gregor Lennie <gregorlennie@hotmail.co.uk>
2020-01-27 16:20:26 +0000

    Adds methods to Printer class, tested with JUnit

--- src/main/java/Printer.java
+++ src/main/java/Printer.java
@@ -1,2 +1,26 @@
 public class Printer {
+
+    private int paper;
+    private int toner;
+
+    public Printer(int paper) {
+        this.paper = paper;
+        this.toner = 20;
+    }
+
+    public int getPaper(){
+        return this.paper;
+    }
+
+    public void print(int numberOfPages, int copies){
+        int pages = numberOfPages * copies;
+        if (this.paper >= pages) {
+            this.paper -= pages;
+            this.toner -= pages;
+        }
+    }
+
+    public int getToner(){
+        return this.toner;
+    }
 }
