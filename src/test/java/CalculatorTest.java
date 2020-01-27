146773124384ce680101edbd4e6c88443f30ee39
Gregor Lennie <gregorlennie@hotmail.co.uk>
2020-01-27 15:21:57 +0000

    Adds tests for Calculator class

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,2 +1,33 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class CalculatorTest {
+
+    private Calculator calculator;
+
+    @Test
+    public void before(){
+        calculator = new Calculator();
+    }
+
+    @Test
+    public void canAdd(){
+        assertEquals(5, calculator.add(2,3));
+    }
+
+    @Test
+    public void canSubtract(){
+        assertEquals(1, calculator.subtract(3,2));
+    }
+
+    @Test
+    public void canMultiply(){
+        assertEquals(8, calculator.multiply(2, 4));
+    }
+
+    @Test
+    public void canDivide(){
+        assertEquals(4.0, calculator.divide(8.0, 2.0), 0.0);
+    }
 }
